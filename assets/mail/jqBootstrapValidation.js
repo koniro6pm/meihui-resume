a20ac71b24bf0e5b1c982dd28667126a47eecb95
Ivy Chen <koniro6pm@gmail.com>
2021-01-17 15:20:47 +0800

    add photo,add english version

--- assets/mail/jqBootstrapValidation.js
+++ assets/mail/jqBootstrapValidation.js
@@ -482,7 +482,7 @@
                     ( settings.options.prependExistingHelpBlock ? $helpBlock.data("original-contents") : "" ));
                 } else {
                   // Multiple? Being sloppy? Glue them together into an UL.
-                  $helpBlock.html("<ul role=\"alert\"><li>" + errorsFound.join("</li><li>") + "</li></ul>" +
+                  $helpBlock.html("<ul role=\"alert\"><p>" + errorsFound.join("</p><p>") + "</p></ul>" +
                     ( settings.options.prependExistingHelpBlock ? $helpBlock.data("original-contents") : "" ));
                 }
               } else {
